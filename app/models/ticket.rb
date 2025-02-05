8332bacd160d60100ff52a63a6ddd3744538e5a4
bubsworth <bubbard96@hotmail.com>
2025-02-05 14:13:42 +0000

    Refresh the view when another user makes changes

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,3 +1,8 @@
 class Ticket < ApplicationRecord
+  broadcasts_refreshes
   validates_presence_of :title, :description
+
+  after_update_commit -> { broadcast_refresh_later_to("main-board") }
+  after_create_commit -> { broadcast_refresh_later_to("main-board") }
+  after_destroy_commit -> { broadcast_refresh_later_to("main-board") }
 end
