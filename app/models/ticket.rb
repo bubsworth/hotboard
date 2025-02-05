d0276fc180416a381cf008172efbb87a6c486309
bubsworth <bubbard96@hotmail.com>
2025-02-05 14:31:59 +0000

    Add state to the tickets

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -2,6 +2,8 @@ class Ticket < ApplicationRecord
   broadcasts_refreshes
   validates_presence_of :title, :description
 
+  enum :state, %i[backlog next in_progress done], default: :backlog
+
   after_update_commit -> { broadcast_refresh_later_to("main-board") }
   after_create_commit -> { broadcast_refresh_later_to("main-board") }
   after_destroy_commit -> { broadcast_refresh_later_to("main-board") }
--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_01_30_113844) do
+ActiveRecord::Schema[8.0].define(version: 2025_02_05_143109) do
   create_table "tickets", force: :cascade do |t|
     t.string "title"
     t.string "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "state", default: 0, null: false
   end
-
 end
