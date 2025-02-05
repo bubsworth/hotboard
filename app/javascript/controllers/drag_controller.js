5d2503db7311d8af6615b1659944a4b0a0fa6cc0
bubsworth <bubbard96@hotmail.com>
2025-02-05 15:34:44 +0000

    Wiring up the events for dragging

--- app/javascript/controllers/drag_controller.js
+++ app/javascript/controllers/drag_controller.js
@@ -1,5 +1,15 @@
-import { Controller } from "@hotwired/stimulus"
-
 export default class extends Controller {
   static targets = ["draggable", "dropzone"]
+  dropzoneTargetConnected(element) {
+    element.addEventListener("dragover", (event) => event.preventDefault())
+    element.addEventListener("drop", this.drop)
+  }
+
+  drop = () => {
+    console.log("DROPPED")
+  }
+
+  start(_event) {
+    console.log("Started dragging")
+  }
 }
